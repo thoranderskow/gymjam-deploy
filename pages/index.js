9d58f09b5f487e41568b3d5ad36ba5a33c87a9b9
thoranderskow <41391604+thoranderskow@users.noreply.github.com>
2020-07-09 14:31:07 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ class Login extends React.Component {
       backgroundColor: 'red',
       justifyContent: 'flex-end',
     };
+    console.log('yo');
     console.log(process.env.VERCEL_URL);
     return(
       <container className='flex'>
