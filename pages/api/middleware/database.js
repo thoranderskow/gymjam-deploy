21fcdae9b5dfcf018150c0a8b68221cd5f70a67d
thoranderskow <41391604+thoranderskow@users.noreply.github.com>
2020-07-09 14:12:46 -0700

    Update database.js

--- pages/api/middleware/database.js
+++ pages/api/middleware/database.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
 
-const client = new MongoClient("mongodb+srv://Tskow:NA3t9BBCv2W6EA2F@cluster-gj.rgowv.mongodb.net/gjdb?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true", {
+const client = new MongoClient("mongodb+srv://Tskow:p9syhPStjvx5tZeW@cluster-gj.rgowv.mongodb.net/gjdb?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
